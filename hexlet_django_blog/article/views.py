aaf60c76a2959de8c6addffb0f6ef9a5fb7c84d3
ibrg <romanbezh@gmail.com>
2023-01-18 22:49:14 +0200

    add search article by title

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views import View
 from django.views.decorators.http import require_http_methods
+from django.db.models import Q
 from .models import Article
 
 
@@ -15,17 +16,13 @@ class IndexView(View):
     template_name = 'article/article.html'
 
     def get(self, request, *args, **kwargs):
+        query =  request.GET.get('query', '')
+        articles = Article.objects.filter(Q(name__icontains=query))
+        return render(request, self.template_name, {'articles': articles})
         
-        return render(request, self.template_name, {
-            'title': self.title,
-            'articles': get_list_or_404(Article)
-            })
-
 
 class ArticleView(View):
     
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['article_id'])
         return render(request, 'article/article_detail.html', {'article': article})
-
-    
