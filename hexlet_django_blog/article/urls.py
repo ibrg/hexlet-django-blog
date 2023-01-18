02d0afa5f314eb8b18f67bc0ed3d7ba8975e5a4a
ibrg <romanbezh@gmail.com>
2023-01-18 23:18:51 +0200

    add method create article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,5 +5,6 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles_index'),
     path('<str:tags>/<int:article_id>', views.index, name='article'),
-    path('<int:article_id>/', views.ArticleView.as_view(), name='article_detail')
+    path('<int:article_id>/', views.ArticleView.as_view(), name='article_detail'),
+    path('create/', views.ArticleCreateView.as_view(), name='article_create')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.views import View
 from django.views.decorators.http import require_http_methods
 from django.db.models import Q
+from django.contrib import messages
 from .models import Article
+from .forms import ArticleForm
 
 
 def index(request, tags, article_id):
@@ -26,3 +28,19 @@ class ArticleView(View):
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['article_id'])
         return render(request, 'article/article_detail.html', {'article': article})
+
+
+class ArticleCreateView(View):
+    
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'article/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        article_name = form.data['name']
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS,  f"Articele {article_name} crested")
+            return redirect('articles_index')
+        return render(request, 'article/create.html', {'form': form})
